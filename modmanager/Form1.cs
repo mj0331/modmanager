229a7ecc099b605397448f090a27d74f1c0dae64
mj0331 <marius.orban99@gmail.com>
2016-08-24 15:55:46 +0300

    Moved the custom menu renderer to it's own file

--- modmanager/Form1.cs
+++ modmanager/Form1.cs
@@ -478,44 +478,4 @@ namespace modmanager
 			Process.Start(ActiveProfile.GamePath);
 		}
 	}
-
-	public class MenuRenderer : ToolStripProfessionalRenderer
-	{
-		public MenuRenderer() : base(new MenuColorTable())
-		{
-
-		}
-	}
-
-	public class MenuColorTable : ProfessionalColorTable
-	{
-		public override Color MenuItemSelected
-		{
-			get { return Color.LightGray; }
-		}
-		public override Color MenuItemBorder
-		{
-			get { return Color.LightGray; }
-		}
-
-		public override Color MenuItemSelectedGradientBegin
-		{
-			get { return Color.LightGray; }
-		}
-		public override Color MenuItemSelectedGradientEnd
-		{
-			get { return Color.LightGray; }
-		}
-
-		public override Color MenuItemPressedGradientBegin
-		{
-			get { return Color.LightGray; }
-		}
-		public override Color MenuItemPressedGradientEnd
-		{
-			get { return Color.LightGray; }
-		}
-
-	}
-
 }
